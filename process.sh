846d25f3bdb0a1a3f068c183671fc9437ebcaee9
itis2010me <itis2010me@gmail.com>
2021-07-17 11:55:27 -0700

    Updated script

--- process.sh
+++ process.sh
@@ -4,17 +4,20 @@
 QUIET=0
 SHATTER=0
 SAT=0
+SAT_TEST=0
 SOLVER="satch"
-
+LOWER_BOUND=0
+UPPER_BOUND=0
+MID=0
+ANS=-1
+k=$1 # colorings
+A=$2 # system of equation
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: ./process.sh <Directory of CNFS> <Results Directory> -flags"
-    echo "flags:"
-    echo "-q|--quiet    Quite mode for the SAT solvers. Glucose does not have quite mode!"
-    echo "-s|--shatter  User Shatter to optimize input files."
-    echo "-sat=<solver> Specifiy the SAT solver you want to use"
-    echo "Also make sure that SAT solver is in the first directory and named SAT"
-    echo "Does not support more specialized flags of each SAT solver, yet!"
+    echo "Usage: ./process.sh k A -lb=x -up=y"
+    echo "k      ->  number of colors"
+    echo "A      ->  system of equations"
+    echo "-lb/up ->  optional lower/upper bound"
     exit 0
 fi
 
@@ -22,74 +25,116 @@ fi
 for arg in "$@"
 do
     case $arg in
-    -q|--quiet)
-    QUIET=1
-    # shift
-    ;;
-    -s|--shatter)
-    SHATTER=1
-    ;;
+    # -q|--quiet)
+    # QUIET=1
+    # ;;
+    # -s|--shatter)
+    # SHATTER=1
+    # ;;
     -sat=*)
     SOLVER="${arg#*=}"
+    ;;
+    -lb=*)
+    LOWER_BOUND=${arg#*=}
+    ;;
+    -up=*)
+    UPPER_BOUND=${arg#*=}
     ;;
     esac
 done
 
+n=$LOWER_BOUND
 
-# Create result directory
-if [ -d "$2" ]
-then
-    echo "$2 directory already exist."
-else
-    echo "Creating results directory..."
-    mkdir $2
-fi
-
-# Check input CNF directory
-if [ ! -d "./$1" ]
-then
-    echo "$1 DOES NOT EXIST!"
-    exit 1
-fi
+echo "----------- [ Start ] ----------"
 
 # RUN Shatter and/or SAT solver
-cd ./$1
+cd ./Rado_CNFs
 
-if [ $SHATTER -eq 1 ]
-then 
-    echo "Running Shatter..."
-fi
-
-for FILE in *.cnf
-do
-    if [ $SHATTER -eq 1 ]
-    then 
-        perl shatter.pl $FILE
-        rm -f *.cnf.g
-        rm -f *.cnf.txt
-    fi
-done
 
 echo "Running solver..."
-for FILE in *.cnf
-do 
-    
-    if [[ $QUIET -eq 0 ]] 
-    then
-        echo "---- [ $FILE ] -------------------"
-        ./$SOLVER $FILE
-        ./$SOLVER $FILE >> ../$2/$FILE.txt
-        echo ""
-    else
-        ./$SOLVER -q $FILE > ../$2/$FILE.txt
-        SAT=$(grep -c 'UNSATISFIABLE' ../$2/$FILE.txt)
-        if [[ $SAT -eq 0 ]]
-        then 
-            echo "$FILE is satisfiable"
-        else    
-            echo "$FILE is unsatisfiable"
-        fi
+
+# ./mapleSCIP.sh 500 3 [2,-2,-7]
+
+
+# check upper bound
+echo "Checking upper bound..."
+./mapleSCIP.sh $UPPER_BOUND $k $A >> log.txt
+FILE=$(find . -name "*.cnf")
+./satch -q $FILE > $FILE.txt
+SAT_TEST=$(grep -c 'UNSATISFIABLE' ./$FILE.txt)
+
+if [[ $SAT_TEST -eq 0 ]]
+then # satisfiable
+    echo "Upper bound too low."
+    echo "Rado number for $A with $k-coloring is > $UPPER_BOUND."
+    exit 0
+fi
+
+# clean up
+rm -f *.cnf
+rm -f *.cnf.txt
+
+
+# Searching
+echo "Searching..."
+
+while [ $UPPER_BOUND -ge $LOWER_BOUND ]
+do
+    MID=$((($LOWER_BOUND+$UPPER_BOUND)/2))
+    echo $MID
+    # Checking satisfiable or not
+    ./mapleSCIP.sh $MID $k $A >> log.txt
+    FILE=$(find . -name "*.cnf")
+    ./satch -q $FILE > $FILE.txt
+    SAT=$(grep -c 'UNSATISFIABLE' ./$FILE.txt)
+
+    if [[ $SAT -eq 0 ]]
+    then # satisfiable
+        LOWER_BOUND=$(($MID+1))
+    else # unsatisfibale
+        ANS=$MID
+        UPPER_BOUND=$(($MID-1))
     fi
+
+    # clean up
+    rm -f *.cnf
+    rm -f *.cnf.txt
 done
 
+if [[ $ANS -eq -1 ]]
+then # satisfiable
+    echo "Cannot find Rado number."
+    exit 0
+fi
+
+echo "" > log.txt
+echo "----------- [ Results ] ----------"
+echo "Rado number for $A with $k-coloring is = $ANS."
+echo "----------- [ Done ] -------------"
 exit 0
+
+
+# for FILE in *.cnf
+# do
+
+#     if [[ $QUIET -eq 0 ]]
+#     then
+#         echo "---- [ $FILE ] -------------------"
+#         ./SAT $FILE
+#         ./SAT $FILE > ../$2/$FILE.txt
+#         echo ""
+#     else
+#         ./SAT -q $FILE > ../$2/$FILE.txt
+#     fi
+# done
+
+# for FILE in *.cnf
+# do
+#     SAT=$(grep -c 'UNSATISFIABLE' ../$2/$FILE.txt)
+#     if [[ $SAT -eq 0 ]]
+#     then
+#         echo "$FILE is satisfiable"
+#     else
+#         echo "$FILE is unsatisfiable"
+#     fi
+# done
